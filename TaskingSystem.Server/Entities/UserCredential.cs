981f0f0bef12778b70b56b98a8a168e2617e49c6
jefer <devjam@elsolnec.org>
2024-10-21 17:30:32 -0500

    Add IValidatableObject to UserCredential

--- TaskingSystem.Server/Entities/UserCredential.cs
+++ TaskingSystem.Server/Entities/UserCredential.cs
@@ -1,9 +1,28 @@
 ﻿namespace TaskingSystem.Server.Entities
 {
-    public class UserCredential
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class UserCredential : IValidatableObject
     {
         public string Email { get; set; }
         public string Password { get; set; }
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> errors = [];
+            string messageRequired = "Field is required";
+
+            if (string.IsNullOrEmpty(Email))
+            {
+                errors.Add(new ValidationResult(messageRequired, [nameof(Email)]));
+            }
+            if (string.IsNullOrEmpty(Password))
+            {
+                errors.Add(new ValidationResult(messageRequired, [nameof(Password)]));
+            }
+
+            return errors;
+        }
     }
 }
